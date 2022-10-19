e0e675d44922c51cd31ea6dc03d24684659982b7
M <M@DESKTOP-D0SAI22>
2022-10-19 15:07:01 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Hossu_Maria_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+  
+        public string FirtsName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace Hossu_Maria_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
