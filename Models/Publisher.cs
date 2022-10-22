e7366fe7e942be88c4b1516c32690a50a741bfca
Maria Hossu <hossu.maria10@gmail.com>
2022-10-22 12:43:58 +0300

    Deleted old project

--- Models/Publisher.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hossu_Maria_Lab2.Models
-{
-    public class Publisher
-    {
-        public int ID { get; set; }
-        public string PublisherName { get; set; }
-        public ICollection<Book>? Books { get; set; }
-    }
-}
